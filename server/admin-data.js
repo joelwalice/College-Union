ab5fc7d0520314539334efec8ff400626f6bb86a
joelwalice <joelwalice@gmail.com>
2024-03-29 18:43:27 +0530

    4

--- server/admin-data.js
+++ server/admin-data.js
@@ -3,7 +3,7 @@ const adminSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true},
     password: String,
-    role: [String],
+    role: String,
 });
 
 const admin = mongoose.model('Admin-Data', adminSchema);
