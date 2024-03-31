b672e81cbe5a4b8d2d5352dc4edb1a36a5973614
JoelWalice <125018037@sastra.ac.in>
2024-03-31 06:10:58 +0530

    12

--- client/src/app/home/student/page.js
+++ client/src/app/home/student/page.js
@@ -159,7 +159,7 @@ export default function Home() {
           {/* Coming Soon */}
           <div className='flex flex-col items-center justify-center p-4'>
             <h1 className="m-4 text-3xl md:text-4xl font-semibold text-white">Coming Soon</h1>
-            <div className='flex items-center p-4 rounded-lg bg-gray-700 min-w-screen md:m-4 gap-2'>
+            <div className='flex items-center p-4 rounded-lg bg-gray-700 min-w-screen grid grid-cols-2 sm:grid-cols-3 md:m-4 gap-2'>
               {["TBI Network", "Tech Community", "Consulting Clubs", "Tech Agency Franchise", "Cultural Community", "Corporate Scouting Network", "Hiring portal", "Interviews prep Community", "Competitive Exam Prep", "Common Interest community", "Dating network"].map((item, index) => (
                 <div key={index} className='flex flex-col items-center justify-center rounded-lg bg-gray-600 p-4'>
                   <h2 className="m-4 text-2xl md:text-2xl font-semibold text-white">{item}</h2>
